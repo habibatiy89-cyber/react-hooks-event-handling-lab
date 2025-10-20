a1c10fa4f2911167e155ccc74929e2e057faaf7f
habiba hussein <habiba.hussein@moringaschool.comclear>
2025-10-20 15:57:02 +0300

    react event handling

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,20 @@
-// Code Keypad Component Here
+import React from "react";
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+function Keypad() {
+  // Event handler for input change
+  function handleChange() {
+    console.log("Entering password...");
+  }
+
+  return (
+    <div>
+      <input
+        type="password"
+        onChange={handleChange} // Attach event handler
+        placeholder="Enter password"
+      />
+    </div>
+  );
 }
 
 export default Keypad;
